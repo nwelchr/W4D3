6da96f749ac00550970f2935c339d0f06efc1b44
Nick Welch <nwelchr@gmail.com>
2018-03-07 14:42:05 -0800

    Add timestamps to Users table

--- ninety_nine_cats/db/schema.rb
+++ ninety_nine_cats/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307213030) do
+ActiveRecord::Schema.define(version: 20180307223812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20180307213030) do
     t.string "user_name", null: false
     t.string "password_digest", null: false
     t.string "session_token", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
 
