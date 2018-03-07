039c21a0b3307e50f26b7d9c329c2d3e365ecfaa
Nick Welch <nwelchr@gmail.com>
2018-03-07 14:58:18 -0800

    Allow access to logged_in? method under views

--- ninety_nine_cats/app/controllers/sessions_controller.rb
+++ ninety_nine_cats/app/controllers/sessions_controller.rb
@@ -4,15 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_credentials(
+    @current_user = User.find_by_credentials(
       params[:user][:user_name],
       params[:user][:password]
     )
 
-    if user
-      # TODO: log_in!(user)
-      user.reset_session_token!
-      self.session[:session_token] = user.session_token
+    if @current_user
+      log_in!(@current_user)
 
       flash[:success] = "Successfully logged in"
       redirect_to cats_url
@@ -29,8 +27,14 @@ class SessionsController < ApplicationController
   end
 
   def log_out!
-    current_user.reset_session_token!
+    @current_user.reset_session_token!
     self.session[session_token] = nil
+  end
+
+  def log_in!(user)
+    #force other clients to log out by regenerating token
+    user.reset_session_token!
+    self.session[:session_token] = user.session_token
   end
 
 end
--- ninety_nine_cats/app/controllers/users_controller.rb
+++ ninety_nine_cats/app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
 
     if user.save
+      
       flash[:success] = "Welcome to Cat Land!"
       redirect_to user_url(user)
     else
